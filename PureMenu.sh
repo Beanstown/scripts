d549906f41c8fa8ca46f397690ea7c081a29b6ed
BeansTown106 <nbenis106@gmail.com>
2016-06-18 11:05:41 -0400

    update puremenu for single release mm2

--- PureMenu.sh
+++ PureMenu.sh
@@ -16,8 +16,7 @@
 
 #Variables
 OUTDIR=~/Android/Completed
-PURELAYERS=~/Android/aosp/purenexus
-PURECMTE=~/Android/aosp/purenexuscmte
+PURE=~/Android/aosp/PureNexusMM2
 FTPSERVER=uploads.androidfilehost.com
 LOGIN=BeansTown106
 PASSWORD=password
@@ -30,75 +29,39 @@ reset=`tput sgr0`
 
 
 #functions
-layers() {
+purenexus() {
   #Prepare
-  cd $PURELAYERS
+  cd $PURE
   source build/envsetup.sh
   repo sync -j8
   mka clean
   #Angler
   brunch angler
-  mv $PURELAYERS/out/target/product/angler/pure_nexus_angler-*.zip $OUTDIR
+  mv $PURE/out/target/product/angler/pure_nexus_angler-*.zip $OUTDIR
   mka clean
   #Bullhead
   brunch bullhead
-  mv $PURELAYERS/out/target/product/bullhead/pure_nexus_bullhead-*.zip $OUTDIR
+  mv $PURE/out/target/product/bullhead/pure_nexus_bullhead-*.zip $OUTDIR
   mka clean
   #Deb
   brunch deb
-  mv $PURELAYERS/out/target/product/deb/pure_nexus_deb-*.zip $OUTDIR
+  mv $PURE/out/target/product/deb/pure_nexus_deb-*.zip $OUTDIR
   mka clean
   #Flo
   brunch flo
-  mv $PURELAYERS/out/target/product/flo/pure_nexus_flo-*.zip $OUTDIR
+  mv $PURE/out/target/product/flo/pure_nexus_flo-*.zip $OUTDIR
   mka clean
   #Flounder
   brunch flounder
-  mv $PURELAYERS/out/target/product/flounder/pure_nexus_flounder-*.zip $OUTDIR
+  mv $PURE/out/target/product/flounder/pure_nexus_flounder-*.zip $OUTDIR
   mka clean
   #Hammerhead
   brunch hammerhead
-  mv $PURELAYERS/out/target/product/hammerhead/pure_nexus_hammerhead-*.zip $OUTDIR
+  mv $PURE/out/target/product/hammerhead/pure_nexus_hammerhead-*.zip $OUTDIR
   mka clean
   #Shamu
   brunch shamu
-  mv $PURELAYERS/out/target/product/shamu/pure_nexus_shamu-*.zip $OUTDIR
-  mka clean
-}
-
-cmte() {
-  #Prepare
-  cd $PURECMTE
-  source build/envsetup.sh
-  repo sync -j8
-  mka clean
-  #Angler
-  brunch angler
-  mv $PURECMTE/out/target/product/angler/pure_nexus_angler-*.zip $OUTDIR
-  mka clean
-  #Bullhead
-  brunch bullhead
-  mv $PURECMTE/out/target/product/bullhead/pure_nexus_bullhead-*.zip $OUTDIR
-  mka clean
-  #Deb
-  brunch deb
-  mv $PURECMTE/out/target/product/deb/pure_nexus_deb-*.zip $OUTDIR
-  mka clean
-  #Flo
-  brunch flo
-  mv $PURECMTE/out/target/product/flo/pure_nexus_flo-*.zip $OUTDIR
-  mka clean
-  #Flounder
-  brunch flounder
-  mv $PURECMTE/out/target/product/flounder/pure_nexus_flounder-*.zip $OUTDIR
-  mka clean
-  #Hammerhead
-  brunch hammerhead
-  mv $PURECMTE/out/target/product/hammerhead/pure_nexus_hammerhead-*.zip $OUTDIR
-  mka clean
-  #Shamu
-  brunch shamu
-  mv $PURECMTE/out/target/product/shamu/pure_nexus_shamu-*.zip $OUTDIR
+  mv $PURE/out/target/product/shamu/pure_nexus_shamu-*.zip $OUTDIR
   mka clean
 }
 
@@ -113,31 +76,18 @@ INPUT_END
 }
 
 testbuilds() {
-  #Prep Layers
-  cd $PURELAYERS
+  #Prep
+  cd $PURE
   source build/envsetup.sh
   repo sync -j8
   mka clean
   #Angler
   brunch angler
-  mv $PURELAYERS/out/target/product/angler/pure_nexus_angler-*.zip $OUTDIR
+  mv $PURE/out/target/product/angler/pure_nexus_angler-*.zip $OUTDIR
   mka clean
   #Shamu
   brunch shamu
-  mv $PURELAYERS/out/target/product/shamu/pure_nexus_shamu-*.zip $OUTDIR
-  mka clean
-  #Prep Cmte
-  cd $PURECMTE
-  source build/envsetup.sh
-  repo sync -j8
-  mka clean
-  #Angler
-  brunch angler
-  mv $PURECMTE/out/target/product/angler/pure_nexus_angler-*.zip $OUTDIR
-  mka clean
-  #Shamu
-  brunch shamu
-  mv $PURECMTE/out/target/product/shamu/pure_nexus_shamu-*.zip $OUTDIR
+  mv $PURE/out/target/product/shamu/pure_nexus_shamu-*.zip $OUTDIR
   mka clean
 }
 
@@ -149,13 +99,11 @@ echo "${red}=========================================================${reset}"
 echo "${red}==${reset}${green}               PureNexus Release Script              ${reset}${red}==${reset}"
 echo "${red}==${reset}${green}       Lets get Ready To Build For The Masses!       ${reset}${red}==${reset}"
 echo "${red}=========================================================${reset}"
-echo "${red}==${reset}${yellow}   1 - Full Release and Upload (Layers & CMTE)       ${reset}${red}==${reset}"
+echo "${red}==${reset}${yellow}   1 - Full Release and Upload                       ${reset}${red}==${reset}"
 echo "${red}==${reset}${yellow}   2 - Full Release without Upload                   ${reset}${red}==${reset}"
-echo "${red}==${reset}${yellow}   3 - Build All devices for CMTE branch             ${reset}${red}==${reset}"
-echo "${red}==${reset}${yellow}   4 - Build All devices for Layers branch           ${reset}${red}==${reset}"
-echo "${red}==${reset}${yellow}   5 - Build Shamu/Angler Test Builds                ${reset}${red}==${reset}"
-echo "${red}==${reset}${yellow}   6 - Upload all files in Out Directory             ${reset}${red}==${reset}"
-echo "${red}==${reset}${yellow}   7 - Delete all files in Out Directory             ${reset}${red}==${reset}"
+echo "${red}==${reset}${yellow}   3 - Build Shamu/Angler Test Builds                ${reset}${red}==${reset}"
+echo "${red}==${reset}${yellow}   4 - Upload all files in Out Directory             ${reset}${red}==${reset}"
+echo "${red}==${reset}${yellow}   5 - Delete all files in Out Directory             ${reset}${red}==${reset}"
 echo "${red}==${reset}${yellow}   0 - Exit                                          ${reset}${red}==${reset}"
 echo "${red}=========================================================${reset}"
 echo ""
@@ -166,8 +114,7 @@ case $menu in
 1 )
   #Full release and upload
   BEGIN=$(date +%s)
-  layers
-  cmte
+  purenexus
   upload
   END=$(date +%s)
   echo "${green}Full Release and Upload Complete!!${reset}"
@@ -178,8 +125,7 @@ case $menu in
 2 )
   #Full release local
   BEGIN=$(date +%s)
-  layers
-  cmte
+  purenexus
   END=$(date +%s)
   echo "${green}Full Release Complete!!${reset}"
   echo "${green}Total time elapsed: $(echo $(($END-$BEGIN)) | awk '{print int($1/60)"mins "int($1%60)"secs "}')${reset}"
@@ -187,26 +133,6 @@ case $menu in
 #############################################################
 
 3 )
-  #Build CMTE locally
-  BEGIN=$(date +%s)
-  cmte
-  END=$(date +%s)
-  echo "${green}CMTE Builds Complete!!${reset}"
-  echo "${green}Total time elapsed: $(echo $(($END-$BEGIN)) | awk '{print int($1/60)"mins "int($1%60)"secs "}')${reset}"
-;;
-#############################################################
-
-4 )
-  #Build Layers locally
-  BEGIN=$(date +%s)
-  layers
-  END=$(date +%s)
-  echo "${green}Layers Builds Complete!!${reset}"
-  echo "${green}Total time elapsed: $(echo $(($END-$BEGIN)) | awk '{print int($1/60)"mins "int($1%60)"secs "}')${reset}"
-;;
-#############################################################
-
-5 )
   #Build Angler/Shamu Test Builds
   BEGIN=$(date +%s)
   testbuilds
@@ -216,7 +142,7 @@ case $menu in
 ;;
 #############################################################
 
-6 )
+4 )
   #Upload OUTDIR
   BEGIN=$(date +%s)
   upload
@@ -226,7 +152,7 @@ case $menu in
 ;;
 #############################################################
 
-7 )
+5 )
   #Wipe contents of OUTDIR
   rm -rf $OUTDIR/*
   echo "${green}Wiped OUTDIR!!${reset}"
